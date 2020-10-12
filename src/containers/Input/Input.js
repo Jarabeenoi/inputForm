d0518e6edf975c16d67409317606cf3f6f811824
jarabeenoi <nongnat61@gmail.com>
2020-10-12 16:33:46 +0700

    inputForm

--- src/containers/Input/Input.js
+++ src/containers/Input/Input.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
 export default class Input extends Component {
+
+   getClassCSS = () => {
+      let cssClass = "Input InputElement";
+      if (this.props.error.status && this.props.error.isTouched) {
+         cssClass += " Invalid";
+      }
+      return cssClass;
+   }
+
    render() {
+      const {value, onChangeInput, name, placeholder} = this.props;
+
       return (
-         <div>
-            
-         </div>
+         <>
+            <input 
+            value={value} 
+            onChange={onChangeInput} 
+            className={this.getClassCSS()} 
+            name={name}
+            placeholder={placeholder} />
+            <p className="ErrorMessage">{this.props.error.message}</p>
+         </>
       )
    }
 }
--- src/containers/RegisterForm/RegisterForm.js
+++ src/containers/RegisterForm/RegisterForm.js
@@ -1,44 +1,119 @@
 import React, { Component } from 'react'
+import Input from '../Input/Input';
 
 export default class RegisterForm extends Component {
    state = {
-      name: "",
-      phonenumber: "",
-      email: "",
-      password: "",
+      formData: {
+         name: {
+            value: "",
+            validator: {
+               minLength: 3,
+               maxLength: 6,
+               required: true
+            },
+            error: { status: true, message: "", isTouched: false }
+         },
+
+         phonenumber: {
+            value: "",
+            validator: {
+               minLength: 10,
+               maxLength: 10,
+            },
+            error: { status: true, message: "", isTouched: false }
+         },
+         email: {
+            value: "",
+            validator: {
+               required: true
+            },
+            error: { status: true, message: "", isTouched: false }
+         },
+         password: {
+            value: "",
+            validator: {
+               minLength: 6,
+               maxLength: 24,
+               required: true
+            },
+            error: { status: true, message: "", isTouched: false }
+         },
+
+      },
+      isFormValid: false
+   };
+
+   checkValue = (value, rules) => {
+      let isValid = true;
+      let trimmedValue = value.trim();
+      let message = "";
+
+      if (rules.require && trimmedValue.length === 0) {
+         isValid = false;
+         message = "คุณต้องกรอกที่นี่";
+      }
+
+      if (rules.maxLength && trimmedValue.length > rules.maxLength) {
+         isValid = false;
+         message = `ช่องนี้ความยาวต้องไม่เกิน ${rules.maxLength} ตัว`;
+      }
+
+      if (rules.minLength && trimmedValue.length < rules.minLength) {
+         isValid = false;
+         message = `ช่องนี้ความยาวต้องไม่เกิน ${rules.minLength} ตัว`;
+      }
+
+      return { isValid, message };
    };
 
    onChangeInput = (e) => {
       const fieldName = e.target.name;
       const fieldValue = e.target.value;
-      console.log("fieldName", fieldName, "fieldValue", fieldValue);
+      const formUpdate = { ...this.state.formData };
+      formUpdate[fieldName].value = fieldValue;
+      formUpdate[fieldName].error.isTouched = true;
+
+      let { isValid, message } = this.checkValue(e.target.value, formUpdate[fieldName].validator);
+
+      formUpdate[fieldName].error.status = !isValid;
+      formUpdate[fieldName].error.message = message;
+
+      let newIsFormValid = true;
+      for (let key in formUpdate) {
+         if (formUpdate[key].validator.required) {
+            newIsFormValid = !formUpdate[key].error.status && newIsFormValid;
+         }
+      }
+
       this.setState({
-         [fieldName]: fieldValue
-      })
-   }
+         fromData: formUpdate,
+         isFormValid: newIsFormValid
+      });
+   };
    onSubmit = (e) => {
       e.preventDefault();
-      const { name, phonenumber, email, password } = this.state;
+      const { name, phonenumber, email, password } = this.state.formData;
       const result = {
-         name,
-         phonenumber,
-         email,
-         password,
+         name: name.value,
+         phonenumber: phonenumber.value,
+         email: email.value,
+         password: password.value,
       }
       console.log(result);
    }
 
    render() {
-      const { name, phonenumber, email, password } = this.state;
+      const { name, phonenumber, email, password } = this.state.formData;
 
       return (
          <div className="RegisterForm">
             <form onSubmit={this.onSubmit}>
-               <input value={name} onChange={this.onChangeInput} className="Input InputElement" name="name" placeholder="ชื่อ" />
-               <input value={phonenumber} onChange={this.onChangeInput} className="Input InputElement" name="phonenumber" placeholder="เบอร์โทรศัพย์" />
-               <input value={email} onChange={this.onChangeInput} className="Input InputElement" name="email" placeholder="อีเมล์" />
-               <input type="password" value={password} onChange={this.onChangeInput} className="Input InputElement" name="password" placeholder="รหัสผ่าน" />
-               <button htmlFor="submit" className="Button">Register</button>
+               <Input onChangeInput={this.onChangeInput} value={name.value} name="name" placeholder="ชื่อ" error={name.error} />
+               <Input onChangeInput={this.onChangeInput} value={phonenumber.value} name="phonenumber" placeholder="เบอร์โทรศัพย์" error={phonenumber.error} />
+               <Input onChangeInput={this.onChangeInput} value={email.value} name="email" placeholder="อีเมล์" error={email.error} />
+               <Input onChangeInput={this.onChangeInput} value={password.value} name="password" placeholder="รหัสผ่าน" error={password.error} />
+               
+               <button disabled={!this.state.isFormValid} htmlFor="submit" className="Button">Register</button>
             </form>
          </div>
       );
